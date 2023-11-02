cc99a3a70298cd15617ca4256d7e7af406c24717
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-11-03 02:16:36 +0530

    new updates

--- src/app/(admin)/admin/ads/page.tsx
+++ src/app/(admin)/admin/ads/page.tsx
@@ -1,19 +1,13 @@
-// import AdsManagement from "@/components/admin/ads-modal";
-// import React from "react";
-
-// export default async function page() {
-//   const ads = await getAds();
-//   return <AdsManagement data={ads} />;
-// }
-// async function getAds() {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/ads`, {
-//     next: { tags: ["ads"], revalidate: 500000 },
-//   });
-//   return res.json();
-// }
-
+import AdsManagement from "@/components/admin/ads-modal";
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default async function page() {
+  const ads = await getAds();
+  return <AdsManagement data={ads} />;
+}
+async function getAds() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/ads`, {
+    next: { tags: ["ads"], revalidate: 500000 },
+  });
+  return res.json();
 }
--- src/app/(admin)/admin/config/page.tsx
+++ src/app/(admin)/admin/config/page.tsx
@@ -1,20 +1,14 @@
-// import React from "react";
-// import Cred from "@/components/admin/config-modal";
-// export default async function Page() {
-//   const data = await getAllUsers();
-//   // Define your fake JSON data
-
-//   return <Cred data={data} />;
-// }
-// async function getAllUsers() {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/forms`, {
-//     next: { tags: ["forms"], revalidate: 600000 },
-//   });
-//   return res.json();
-// }
-
 import React from "react";
+import Cred from "@/components/admin/config-modal";
+export default async function Page() {
+  const data = await getAllUsers();
+  // Define your fake JSON data
 
-export default function page() {
-  return <div>page</div>;
+  return <Cred data={data} />;
+}
+async function getAllUsers() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/forms`, {
+    next: { tags: ["forms"], revalidate: 600000 },
+  });
+  return res.json();
 }
