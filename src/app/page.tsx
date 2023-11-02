72ca32fde6e23f7bef7108d127402a2663b4a1a4
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-11-03 00:54:28 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,19 @@
-// import TestForm from "@/components/Test";
-// import { getEventById } from "@/services/events";
-// import React from "react";
-
-// export default async function page() {
-//   const data = await getFormData();
-//   const content = await getEventById("sScBCxKTOVcw3y15jHaM");
-//   return <TestForm data={data} content={content} />;
-// }
-
-// async function getFormData() {
-//   const res = await fetch("http://localhost:3000/api/forms", {
-//     next: { tags: ["forms"], revalidate: 60000 },
-//   });
-//   return res.json();
-// }
-
+import TestForm from "@/components/Test";
+import { getEventById } from "@/services/events";
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default async function page() {
+  const data = await getFormData();
+  const content = await getEventById("sScBCxKTOVcw3y15jHaM");
+  return <TestForm data={data} content={content} />;
+}
+
+async function getFormData() {
+  const res = await fetch(
+    "https://main--shimmering-daifuku-1fac39.netlify.app/api/forms",
+    {
+      next: { tags: ["forms"], revalidate: 60000 },
+    }
+  );
+  return res.json();
 }
