c30bc74488385f00147be27fb78a724eacee1230
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-11-03 01:00:27 +0530

    new changes

--- src/app/(admin)/admin/credentials/page.tsx
+++ src/app/(admin)/admin/credentials/page.tsx
@@ -1,23 +1,17 @@
-// import Cred from "@/components/admin/change-password-modal";
-// import React, { useState } from "react";
+import Cred from "@/components/admin/change-password-modal";
+import React, { useState } from "react";
 
-// export default async function Page() {
-//   const data = await getAllUsers();
-//   // Define your fake JSON data
+export default async function Page() {
+  const data = await getAllUsers();
+  // Define your fake JSON data
 
-//   return <Cred data={data} />;
-// }
+  return <Cred data={data} />;
+}
 
-// // Function to fetch all users from Firestore
-// async function getAllUsers() {
-//   const res = await fetch("http://localhost:3000/api/users", {
-//     next: { tags: ["users"], revalidate: 600000 },
-//   });
-//   return res.json();
-// }
-
-import React from "react";
-
-export default function page() {
-  return <div>page</div>;
+// Function to fetch all users from Firestore
+async function getAllUsers() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/users`, {
+    next: { tags: ["users"], revalidate: 600000 },
+  });
+  return res.json();
 }
