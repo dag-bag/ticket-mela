bcb84aab21ece5028afcc31c383edef1bf3c562c
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-11-03 00:28:13 +0530

    bug fix

--- src/app/(admin)/admin/config/page.tsx
+++ src/app/(admin)/admin/config/page.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import Cred from "@/components/admin/config-modal";
-export default async function Page() {
-  const data = await getAllUsers();
-  // Define your fake JSON data
+// import React from "react";
+// import Cred from "@/components/admin/config-modal";
+// export default async function Page() {
+//   const data = await getAllUsers();
+//   // Define your fake JSON data
 
-  return <Cred data={data} />;
-}
-async function getAllUsers() {
-  const res = await fetch("http://localhost:3000/api/forms", {
-    next: { tags: ["forms"], revalidate: 600000 },
-  });
-  return res.json();
+//   return <Cred data={data} />;
+// }
+// async function getAllUsers() {
+//   const res = await fetch("http://localhost:3000/api/forms", {
+//     next: { tags: ["forms"], revalidate: 600000 },
+//   });
+//   return res.json();
+// }
+
+import React from "react";
+
+export default function page() {
+  return <div>page</div>;
 }
