0a7755b7ca608dccd4a7ba646bbc85019c83d51b
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-11-03 16:38:00 +0530

    Update page.tsx

--- src/app/(admin)/admin/config/page.tsx
+++ src/app/(admin)/admin/config/page.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import Cred from "@/components/admin/config-modal";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../../../../../firebase.config";
 export default async function Page() {
   const data = await getAllUsers();
   // Define your fake JSON data
 
   return <Cred data={data} />;
 }
-async function getAllUsers() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/forms`, {
-    next: { tags: ["forms"], revalidate: 600000 },
-  });
-  return res.json();
+async function getAllUsers(): Promise<any> {
+  try {
+    const usersCollection = collection(db, "forms"); // Replace "users" with your Firestore collection name
+
+    const usersSnapshot = await getDocs(query(usersCollection));
+    const usersData: any = [];
+
+    usersSnapshot.forEach((doc) => {
+      usersData.push({
+        id: doc.id,
+        ...doc.data(),
+      } as any);
+    });
+
+    return usersData;
+  } catch (error) {
+    console.error("Error fetching users: ", error);
+    throw error;
+  }
 }
