e4417908d4eb541d6a1deeaef8d14bed5ae7be16
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-11-03 16:32:15 +0530

    Update page.tsx

--- src/app/(admin)/admin/events/page.tsx
+++ src/app/(admin)/admin/events/page.tsx
@@ -1,16 +1,10 @@
-// import EventForm from "@/components/admin/event-form";
-// import { getEventById } from "@/services/events";
-// import React from "react";
-
-// export default async function page() {
-//   const data = await getEventById("sScBCxKTOVcw3y15jHaM");
-//   return <EventForm event={data} />;
-// }
-
-// export const revalidate = 6000;
-
+import EventForm from "@/components/admin/event-form";
+import { getEventById } from "@/services/events";
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default async function page() {
+  const data = await getEventById("sScBCxKTOVcw3y15jHaM");
+  return <EventForm event={data} />;
 }
+
+export const revalidate = 6000;
